ce40a98a6b18bbbf1dc6d67b6cf2605f2a42c39c
Antonio <rssntn67@yahoo.it>
2018-09-23 22:18:55 +0200

    SubScription Management Dashboard
    Added spring boot nature project
    with vaadin. Added pom and SmsApplication

---
+++ src/main/java/it/arsinfo/smd/SmdApplication.java
@@ -0,0 +1,12 @@
+package it.arsinfo.smd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdApplication.class, args);
+	}
+}
