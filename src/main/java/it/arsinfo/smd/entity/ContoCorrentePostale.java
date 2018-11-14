e357e13aac4eaa45180bb60bb32147ca3b25718d
Antonio <rssntn67@yahoo.it>
2018-11-14 13:11:22 +0100

    Set methid getCcp static

--- src/main/java/it/arsinfo/smd/entity/ContoCorrentePostale.java
+++ src/main/java/it/arsinfo/smd/entity/ContoCorrentePostale.java
@@ -4,12 +4,13 @@ public enum ContoCorrentePostale {
     UNO("000063470009");
     private String ccp; 
     
-    public ContoCorrentePostale getByCcp(String ccp) {
+    public static ContoCorrentePostale getByCcp(String ccp) {
         for (ContoCorrentePostale l : ContoCorrentePostale.values()) {
             if (l.ccp.equals(ccp)) return l;
         }
         throw new IllegalArgumentException("ContoCorrentePostale not found.");
     }
+
     private ContoCorrentePostale(String ccp) {
         this.ccp = ccp;
     }
