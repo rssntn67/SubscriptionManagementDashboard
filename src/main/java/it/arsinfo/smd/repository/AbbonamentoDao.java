73c2f8f59cd0554c8f8b4d5dba1e66fae8fef5b9
Antonio <rssntn67@yahoo.it>
2018-11-15 12:09:22 +0100

    Aggiunta la possibilita di incassare a mano
    Tutti i bollettini o sono automaticamente incassati o
    vengono incassati a mano

--- src/main/java/it/arsinfo/smd/repository/AbbonamentoDao.java
+++ src/main/java/it/arsinfo/smd/repository/AbbonamentoDao.java
@@ -3,6 +3,7 @@ package it.arsinfo.smd.repository;
 import it.arsinfo.smd.entity.Abbonamento;
 import it.arsinfo.smd.entity.Anagrafica;
 import it.arsinfo.smd.entity.Campagna;
+import it.arsinfo.smd.entity.Versamento;
 
 import java.util.List;
 
@@ -12,6 +13,8 @@ public interface AbbonamentoDao extends JpaRepository<Abbonamento, Long> {
 
 	List<Abbonamento> findByAnagrafica(Anagrafica anagrafica);
 	List<Abbonamento> findByCampagna(Campagna campagna);
-	Abbonamento getByCampo(String campo);
+	List<Abbonamento> findByVersamento(Versamento versamento);
+	List<Abbonamento> findByCampo(String campo);
+	List<Abbonamento> findByPagatoAndOmaggio(boolean pagato, boolean omaggio);
 
 }
--- src/main/java/it/arsinfo/smd/vaadin/VersamentoUI.java
+++ src/main/java/it/arsinfo/smd/vaadin/VersamentoUI.java
@@ -4,7 +4,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
@@ -12,6 +15,7 @@ import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
@@ -19,6 +23,9 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.ValoTheme;
 
+import it.arsinfo.smd.SmdApplication;
+import it.arsinfo.smd.entity.Abbonamento;
+import it.arsinfo.smd.entity.TipoDocumentoBollettino;
 import it.arsinfo.smd.entity.Versamento;
 import it.arsinfo.smd.repository.AbbonamentoDao;
 import it.arsinfo.smd.repository.VersamentoDao;
@@ -32,7 +39,12 @@ public class VersamentoUI extends SmdHeader {
      */
     private static final long serialVersionUID = 7884064928998716106L;
 
+    private static final Logger log = LoggerFactory.getLogger(SmdApplication.class);
+
     Grid<Versamento> grid;
+    
+    Grid<Abbonamento> abbonamentiAssociabili;
+    Grid<Abbonamento> abbonamentiAssociati;
     
     @Autowired
     AbbonamentoDao abbRepo;
@@ -40,13 +52,22 @@ public class VersamentoUI extends SmdHeader {
     @Autowired
     VersamentoDao verRepo;
 
+    Versamento versamento;
+    
+    Label residuo; 
+    
+    Label avviso; 
+
     @Override
     protected void init(VaadinRequest request) {
         super.init(request);
 
         Assert.notNull(abbRepo, "repo must be not null");
         Label header = new Label("Versamenti Clienti");
+        residuo = new Label(); 
+        avviso = new Label();
         
+        HorizontalLayout info = new HorizontalLayout(avviso,residuo);
         DateField filterDataContabile = new DateField("Selezionare la data Contabile");
         filterDataContabile.setDateFormat("yyyy-MM-dd");
 
@@ -55,27 +76,119 @@ public class VersamentoUI extends SmdHeader {
 
         TextField filterImporto = new TextField("Inserire Importo");
         grid = new Grid<>(Versamento.class);
+        abbonamentiAssociati = new Grid<>(Abbonamento.class);
+        abbonamentiAssociabili = new Grid<>(Abbonamento.class);
         HorizontalLayout actions = new HorizontalLayout(filterImporto,filterDataPagamento,filterDataContabile);
-        addComponents(header, actions, grid);
+        addComponents(header, info,actions, grid,abbonamentiAssociati,abbonamentiAssociabili);
 
         header.addStyleName(ValoTheme.LABEL_H2);
+        
         grid.setColumns("id", "ccp.ccp","campo","campovalido",              
                         "dataPagamento","dataContabile","importo",
                         "errore",
                         "tipoDocumento.bollettino",
                         "tipoAccettazione.tipo","tipoSostitutivo.descr",
                         "provincia","ufficio","sportello","bobina", "progressivoBobina"
-                                  );
-
+                      );
         grid.setWidth("80%");
-
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            edit(e.getValue());
+        });
+        
         filterDataContabile.addValueChangeListener(e -> listByContabile(e.getValue()));
         filterDataPagamento.addValueChangeListener(e -> listByPagamento(e.getValue()));
         filterImporto.setValueChangeMode(ValueChangeMode.LAZY);
         filterImporto.addValueChangeListener(e -> listByImporto(e.getValue()));
+
+        abbonamentiAssociati.setColumns("anagrafica.cognome", "anagrafica.nome","contoCorrentePostale.ccp","cost","campo","incasso","pagato","omaggio");                
+        abbonamentiAssociati.addComponentColumn(abbonamento -> {
+           Button button = new Button("Dissocia");
+           button.addClickListener(click -> dissocia(abbonamento));
+           button.setEnabled(versamento.getTipoDocumento() != TipoDocumentoBollettino.TIPO674);
+           return button;
+        });
+        abbonamentiAssociati.setWidth("100%");
+        abbonamentiAssociati.setVisible(false);
         
+        abbonamentiAssociabili.setColumns("anagrafica.cognome", "anagrafica.nome","contoCorrentePostale.ccp","cost","campo","incasso","pagato","omaggio");                
+        abbonamentiAssociabili.addComponentColumn(abbonamento -> {
+           Button button = new Button("Associa");
+           button.addClickListener(click -> incassa(abbonamento));
+           button.setEnabled(versamento.getTipoDocumento() != TipoDocumentoBollettino.TIPO674);
+           return button;
+        });
+        abbonamentiAssociabili.setWidth("100%");  
+        abbonamentiAssociabili.setVisible(false);
+        residuo.addStyleName(ValoTheme.LABEL_H3);
+        residuo.setVisible(false);
+        avviso.addStyleName(ValoTheme.LABEL_H3);
+        avviso.setVisible(false);
         list();
 
+    }
+
+    private void dissocia(Abbonamento abbonamento) {
+        abbonamento.setVersamento(null);
+        abbonamento.setPagato(false);
+        abbonamento.setIncasso(null);
+        abbRepo.save(abbonamento);
+        edit(versamento);
+    }
+
+    private void incassa(Abbonamento abbonamento) {
+        abbonamento.setVersamento(versamento);
+        abbonamento.setPagato(true);
+        abbonamento.setIncasso(versamento.getDataPagamento());
+        abbRepo.save(abbonamento);
+        edit(versamento);
+    }
+
+    private void edit(Versamento value) {
+        if (value == null) {
+            abbonamentiAssociabili.setVisible(false);
+            abbonamentiAssociati.setVisible(false);
+            residuo.setVisible(false);
+            avviso.setVisible(false);
+            return;
+        }
+        versamento=value;
+        List<Abbonamento> matching;
+        if (versamento.getTipoDocumento() == TipoDocumentoBollettino.TIPO674) {
+            matching = abbRepo.findByCampo(versamento.getCampo());
+            abbonamentiAssociabili.setVisible(false);
+        } else {
+            matching = abbRepo.findByVersamento(versamento);
+            abbonamentiAssociabili.setItems(abbRepo.findByPagatoAndOmaggio(false, false));
+            abbonamentiAssociabili.setVisible(true);
+        }
+        avviso.setVisible(true);
+        residuo.setVisible(true);
+        
+        if (matching.size() == 0 ) {
+            residuo.setValue("Residuo EUR: " + versamento.getImporto().toString());
+            avviso.setValue("Nessun Abbonamento Trovato Per il Versamento Selezionato");
+            abbonamentiAssociati.setVisible(false);
+            return;
+        } 
+        
+        abbonamentiAssociati.setItems(matching);
+        abbonamentiAssociati.setVisible(true);
+        matching.stream().filter(abbonamento -> !abbonamento.isPagato() && !abbonamento.isOmaggio()).forEach(abbonamento -> {
+                log.info("incasso");
+                log.info(abbonamento.toString());
+                incassa(abbonamento);
+        });
+        BigDecimal diff = versamento.getImporto();
+        for (Abbonamento abbonamento: matching) {
+            diff = diff.subtract(abbonamento.getCost());
+        }
+        residuo.setValue("Residuo EUR: " + diff.toString());
+        
+        if (matching.size() == 1) {
+            avviso.setValue("Trovato " + matching.size() + " Abbonamento Per il Versamento Selezionato");
+        } else {
+            avviso.setValue("Trovati " + matching.size() + " Abbonamenti Per il Versamento Selezionato");
+        }
     }
 
     private void listByImporto(String value) {
