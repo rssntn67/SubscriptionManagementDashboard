0614844fd784ec47fae8226de29c7aaffe7abe5f
Antonio <rssntn67@yahoo.it>
2019-04-04 11:22:32 +0200

    Aggiunti intestazione

--- src/main/java/it/arsinfo/smd/data/TitoloAnagrafica.java
+++ src/main/java/it/arsinfo/smd/data/TitoloAnagrafica.java
@@ -1,12 +1,29 @@
 package it.arsinfo.smd.data;
 
 public enum TitoloAnagrafica {
-	Signor,
-	Signora,
-	Dottore,
-	Spett,
-	Vescovo,
-	VescovoAusiliare,
-        VescovoMetropolita,
-	Cardinale;
+	Signore("Egregio Sig."),
+	Signora("Gentile Sig.ra"),
+	Dottore("Egregio Dott."),
+	Istituto("Spettabile"),
+	Vescovo("S.E. Mons."),
+	VescovoAusiliare("S.E. Mons."),
+        VescovoMetropolita("S.E. Mons."),
+	Cardinale("S.Em. Card.");
+    
+    
+    private String intestazione;
+    
+    private TitoloAnagrafica(String intestazione) {
+        this.intestazione=intestazione;
+    }
+
+    public String getIntestazione() {
+        return intestazione;
+    }
+
+    public void setIntestazione(String intestazione) {
+        this.intestazione = intestazione;
+    }
+    
+    
 }
