0f8e523e8d647f7261ad46e862cd7e389a8a31c8
Antonio <rssntn67@yahoo.it>
2019-04-12 19:48:45 +0200

    Aggiunti log di autenticazione

--- src/main/java/it/arsinfo/smd/security/UserDetailsServiceImpl.java
+++ src/main/java/it/arsinfo/smd/security/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package it.arsinfo.smd.security;
 
 import java.util.Collections;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,29 +11,34 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import it.arsinfo.smd.Smd;
 import it.arsinfo.smd.entity.UserInfo;
 import it.arsinfo.smd.repository.UserInfoDao;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	private final UserInfoDao userService;
+    private static final Logger log = LoggerFactory.getLogger(Smd.class);
+    private final UserInfoDao userService;
 
-	@Autowired
-	public UserDetailsServiceImpl(UserInfoDao userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public UserDetailsServiceImpl(UserInfoDao userService) {
+        this.userService = userService;
+    }
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		UserInfo user = userService.findByUsername(username);
-		if (null == user) {
-			throw new UsernameNotFoundException("No user present with username: " + username);
-		} else {
-			return new org.springframework.security.core.userdetails.User(
-                      user.getUsername(), 
-                      user.getPasswordHash(),
-                      Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name())));
-		}
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        UserInfo user = userService.findByUsername(username);
+        if (null == user) {
+            log.warn("user authentication failed for username: '{}'", username);
+            throw new UsernameNotFoundException("No user present with username: "
+                    + username);
+        } else {
+            log.info("authentication success for username: '{}'", username);
+            return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                                                                          user.getPasswordHash(),
+                                                                          Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name())));
+        }
+    }
 }
