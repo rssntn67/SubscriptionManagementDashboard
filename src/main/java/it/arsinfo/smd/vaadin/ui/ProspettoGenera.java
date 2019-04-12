1527263c9dd288ccef6ef73592947a243d821e15
Antonio <rssntn67@yahoo.it>
2019-04-12 19:33:54 +0200

    Prospetti possono essere aggiunti

--- src/main/java/it/arsinfo/smd/vaadin/ui/ProspettoGenera.java
+++ src/main/java/it/arsinfo/smd/vaadin/ui/ProspettoGenera.java
@@ -13,13 +13,14 @@ import com.vaadin.ui.Notification;
 import it.arsinfo.smd.Smd;
 import it.arsinfo.smd.data.Anno;
 import it.arsinfo.smd.data.Mese;
+import it.arsinfo.smd.data.Omaggio;
 import it.arsinfo.smd.entity.Pubblicazione;
 import it.arsinfo.smd.repository.AbbonamentoDao;
-import it.arsinfo.smd.repository.OperazioneDao;
+import it.arsinfo.smd.repository.ProspettoDao;
 import it.arsinfo.smd.vaadin.model.SmdBox;
 import it.arsinfo.smd.vaadin.model.SmdChangeHandler;
 
-public class OperazioneGenera extends SmdChangeHandler {
+public class ProspettoGenera extends SmdChangeHandler {
 
     private final HorizontalLayout buttons = new HorizontalLayout();
     private final Button genera;
@@ -27,15 +28,10 @@ public class OperazioneGenera extends SmdChangeHandler {
     private final PubblicazioneBox pBox;
     private final SmdBox<Anno> aBox;
     private final SmdBox<Mese> mBox;
-    public OperazioneGenera(String caption, VaadinIcons icon,OperazioneDao operazioneDao, AbbonamentoDao abbonamentoDao, List<Pubblicazione> pubblicazioni) {
+    private final SmdBox<Omaggio> oBox;
+    public ProspettoGenera(String caption, VaadinIcons icon,ProspettoDao prospettoDao, AbbonamentoDao abbonamentoDao, List<Pubblicazione> pubblicazioni) {
         pBox = new PubblicazioneBox(pubblicazioni);
-        final Anno annoCorrente = Smd.getAnnoCorrente();
-        List<Anno> anni = 
-                EnumSet.allOf(Anno.class)
-                .stream()
-                .filter(anno -> anno.getAnno() >= annoCorrente.getAnno())
-                .collect(Collectors.toList());
-         aBox = new SmdBox<Anno>(anni) {
+        aBox = new SmdBox<Anno>(EnumSet.allOf(Anno.class)) {
             
             @Override
             public boolean getReadOnly(Anno t, boolean persisted) {
@@ -59,6 +55,19 @@ public class OperazioneGenera extends SmdChangeHandler {
                 return t.getNomeBreve();
             }
         };
+
+        oBox = new SmdBox<Omaggio>(EnumSet.allOf(Omaggio.class)) {
+            
+            @Override
+            public boolean getReadOnly(Omaggio t, boolean persisted) {
+                return false;
+            }
+            
+            @Override
+            public String getBoxCaption(Omaggio t) {
+                return t.name();
+            }
+        };
         indietro = new Button("indietro");
         indietro.addClickListener(click -> onChange());
         genera = new Button(caption,icon);
@@ -75,29 +84,28 @@ public class OperazioneGenera extends SmdChangeHandler {
                 Notification.show("Selezionare almeno un Mese", Notification.Type.WARNING_MESSAGE);
                 return;
             }
-            aBox.getSelected().stream().forEach(anno -> {
-                Set<Mese> mesi = mBox.getSelected().stream()
-                        .collect(Collectors.toSet());
-                if (anno.getAnno() == annoCorrente.getAnno()) {
-                    Mese meseCorrente = Smd.getMeseCorrente();
-                    mesi = mesi.stream()
-                            .filter(m -> m.getPosizione() > meseCorrente.getPosizione())
-                            .collect(Collectors.toSet());
-                }
-                mesi.stream().forEach(mese -> {
+            if (oBox.getSelected().isEmpty()) {
+                Notification.show("Selezionare almeno un Omaggio", Notification.Type.WARNING_MESSAGE);
+                return;
+            }
+            Set<Mese> mesi = mBox.getSelected().stream()
+                    .collect(Collectors.toSet());
+            Set<Omaggio> omaggi = oBox.getSelected().stream()
+                    .collect(Collectors.toSet());
+            aBox.getSelected().stream().forEach(a -> {
+                omaggi.stream().forEach(o -> {
+                mesi.stream().forEach(m -> {
                     pubblicazioni.stream().forEach(p -> {
-                        operazioneDao.deleteByAnnoAndMeseAndPubblicazione(anno, mese, p);
+                        prospettoDao.deleteByAnnoAndMeseAndPubblicazioneAndOmaggio(a, m, p,o);
                     });
-                });
-                operazioneDao.saveAll(Smd.generaOperazioni(pBox.getSelected(), abbonamentoDao.findByAnno(anno), anno, mesi));
+                });});
+                prospettoDao.saveAll(Smd.generaProspetti(pBox.getSelected(), abbonamentoDao.findByAnno(a), a, mesi,omaggi));
                 onChange();
             });
         });
-        
         buttons.addComponent(genera);
         buttons.addComponent(indietro);
-
-        setComponents(buttons,pBox.getLayout(),aBox.getLayout(),mBox.getLayout());
+        setComponents(buttons,pBox.getLayout(),aBox.getLayout(),mBox.getLayout(),oBox.getLayout());
     }
 
     public void edit() {
@@ -107,6 +115,7 @@ public class OperazioneGenera extends SmdChangeHandler {
         pBox.edit(false);
         aBox.edit(false);
         mBox.edit(false);
+        oBox.edit(false);
     }
     
 }
