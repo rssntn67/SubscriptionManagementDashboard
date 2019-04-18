a4f378002d66eb6d4817d112d7cfd1627923f9cf
Antonio <rssntn67@yahoo.it>
2019-04-18 11:01:26 +0200

    Aggiunto filtro per cassa abbonamento

--- src/main/java/it/arsinfo/smd/repository/SpedizioneDao.java
+++ src/main/java/it/arsinfo/smd/repository/SpedizioneDao.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import it.arsinfo.smd.data.Invio;
+import it.arsinfo.smd.data.Omaggio;
 import it.arsinfo.smd.entity.Abbonamento;
 import it.arsinfo.smd.entity.Anagrafica;
 import it.arsinfo.smd.entity.Pubblicazione;
@@ -14,5 +16,11 @@ public interface SpedizioneDao extends JpaRepository<Spedizione, Long> {
 	List<Spedizione> findByAbbonamento(Abbonamento abbonamento);
         List<Spedizione> findByPubblicazione(Pubblicazione pubblicazione);
         List<Spedizione> findByDestinatario(Anagrafica destinatario);
-
+        List<Spedizione> findByIntestatario(Anagrafica intestatario);
+        List<Spedizione> findByIntestatarioAndDestinatario(Anagrafica intestatario, Anagrafica destinatario);
+        List<Spedizione> findByIntestatarioAndPubblicazione(Anagrafica intestatario, Pubblicazione pubblicazione);
+        List<Spedizione> findByDestinatarioAndPubblicazione(Anagrafica destinatario, Pubblicazione pubblicazione);
+        List<Spedizione> findByIntestatarioAndDestinatarioAndPubblicazione(Anagrafica intestatario, Anagrafica destinatario, Pubblicazione pubblicazione);
+        List<Spedizione> findByOmaggio(Omaggio omaggio);
+        List<Spedizione> findByInvio(Invio invio);
 }
--- src/main/java/it/arsinfo/smd/ui/vaadin/AbbonamentoSearch.java
+++ src/main/java/it/arsinfo/smd/ui/vaadin/AbbonamentoSearch.java
@@ -8,6 +8,7 @@ import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalLayout;
 
 import it.arsinfo.smd.data.Anno;
+import it.arsinfo.smd.data.Cassa;
 import it.arsinfo.smd.entity.Abbonamento;
 import it.arsinfo.smd.entity.Anagrafica;
 import it.arsinfo.smd.entity.Campagna;
@@ -18,6 +19,7 @@ public class AbbonamentoSearch extends SmdSearch<Abbonamento> {
     private Anagrafica customer;
     private Anno anno;
     private Campagna campagna;
+    private final ComboBox<Cassa> filterCassa = new ComboBox<Cassa>("Cassa", EnumSet.allOf(Cassa.class));
 
     public AbbonamentoSearch(AbbonamentoDao abbonamentoDao,
             List<Anagrafica> anagrafica, List<Campagna> campagne) {
@@ -27,7 +29,7 @@ public class AbbonamentoSearch extends SmdSearch<Abbonamento> {
         ComboBox<Anno> filterAnno = new ComboBox<Anno>();
         ComboBox<Campagna> filterCampagna = new ComboBox<Campagna>();
 
-        setComponents(new HorizontalLayout(filterAnagrafica,filterCampagna,filterAnno));
+        setComponents(new HorizontalLayout(filterAnagrafica,filterCampagna,filterAnno,filterCassa));
 
         filterCampagna.setEmptySelectionAllowed(true);
         filterCampagna.setPlaceholder("Cerca per Campagna");
@@ -67,57 +69,69 @@ public class AbbonamentoSearch extends SmdSearch<Abbonamento> {
             }
             onChange();
         });
+        
+        filterCassa.setPlaceholder("Seleziona Cassa");
+        filterCassa.addSelectionListener(e ->onChange());
+
 
     }
 
     @Override
     public List<Abbonamento> find() {
         if (campagna == null && customer == null && anno == null) {
-            return findAll();            
+            return filterAll(findAll());            
         }
         if (campagna == null && anno == null) {
-            return ((AbbonamentoDao) getRepo()).findByIntestatario(customer);
+            return filterAll(((AbbonamentoDao) getRepo()).findByIntestatario(customer));
         }
         if (customer == null && anno == null) {
-            return ((AbbonamentoDao) getRepo()).findByCampagna(campagna);
+            return filterAll(((AbbonamentoDao) getRepo()).findByCampagna(campagna));
         }
         if (customer == null && campagna == null) {
-            return ((AbbonamentoDao) getRepo()).findByAnno(anno);
+            return filterAll(((AbbonamentoDao) getRepo()).findByAnno(anno));
         }
         
         if (anno == null) {
-           return ((AbbonamentoDao) getRepo()).findByIntestatario(customer)
+           return filterAll(((AbbonamentoDao) getRepo()).findByIntestatario(customer)
             .stream()
             .filter(a -> 
                 a.getCampagna() != null
                 && a.getCampagna().getId() == campagna.getId()
             )
-            .collect(Collectors.toList());
+            .collect(Collectors.toList()));
         }
         if (campagna == null) {
-           return ((AbbonamentoDao) getRepo()).findByIntestatario(customer)
+           return filterAll(((AbbonamentoDao) getRepo()).findByIntestatario(customer)
             .stream()
             .filter(a -> 
                 a.getAnno() == anno
                 )
-            .collect(Collectors.toList());
+            .collect(Collectors.toList()));
         }
         if (customer == null) {
-            return ((AbbonamentoDao) getRepo()).findByCampagna(campagna)
+            return filterAll(((AbbonamentoDao) getRepo()).findByCampagna(campagna)
                 .stream()
                     .filter(a -> 
                         a.getAnno() == anno
                         )
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toList()));
         }
-        return ((AbbonamentoDao) getRepo()).findByIntestatario(customer)
+        return filterAll(((AbbonamentoDao) getRepo()).findByIntestatario(customer)
                 .stream()
                 .filter(a -> 
                     a.getCampagna() != null
                     && a.getCampagna().getId() == campagna.getId()
                     && a.getAnno() == anno
                     )
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+    }
+
+    private List<Abbonamento> filterAll(List<Abbonamento> abbonamenti) {
+        if (filterCassa.getValue() != null) {
+            abbonamenti=abbonamenti.stream().filter(a -> a.getCassa() == filterCassa.getValue()).collect(Collectors.toList());      
+        }
+        
+        return abbonamenti;
     }
 
 }
