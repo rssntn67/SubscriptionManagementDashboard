7a1acac3524d154fa0eaca1ae07b9637926ffa5b
Antonio <rssntn67@yahoo.it>
2020-01-21 07:45:26 +0100

    Better echo

--- bin/migrationfrom1to2/check.sh
+++ bin/migrationfrom1to2/check.sh
@@ -1,3 +1,66 @@
+SQL_GET_VERSAMENTI_DUPLICATI="select
+   a.id as abbid,
+   v.id as verid,
+   v.incasso_id as incid,
+   i.cassa,
+   a.code_line as abbcode,
+   v.operazione,
+   a.importo+a.pregresso+a.spese+a.spese_estero as totale,
+   a.incassato as abbinc ,
+   v.importo as verimp
+ from abbonamento a
+ left join versamento v
+ on a.code_line = v.code_line
+ left join incasso i
+ on i.id = v.incasso_id
+ where v.id is not null
+ and a.incassato > 0
+ and v.incassato = 0;"
+
+SQL_CHECK_IMPORTO="select 
+       i.id,
+       i.importo,
+       sum(v.importo) 
+   from incasso i left join versamento v 
+   on v.incasso_id=i.id  
+   group by i.id 
+   order by i.importo desc;"
+         
+echo check incassi con importo maggiore della somma dei versamenti
+psql smd -tAX -c "$SQL_CHECK_IMPORTO" | while read line
+do 
+id=`echo $line | cut -d '|' -f1` 
+inc=`echo $line | cut -d '|' -f2`; 
+ver=`echo $line | cut -d '|' -f3`; 
+if [ "$ver" != "$inc" ]
+ then echo "error: importo errato incassois=$incasso importo=$inc versmaneti=$ver"
+fi
+done
+echo "------------------"
+echo
+
+SQL_CHECK_INCASSATO="select i.id,
+       i.incassato,
+       sum(v.incassato) 
+  from incasso i 
+  left join versamento v on v.incasso_id=i.id  
+  group by i.id 
+  order by i.incassato desc;"
+ 
+echo check incassi con incasso maggiore della somma dei versamenti
+psql smd -tAX -c "$SQL_CHECK_INCASSATO" | while read line
+do 
+id=`echo $line | cut -d '|' -f1` 
+inc=`echo $line | cut -d '|' -f2`; 
+ver=`echo $line | cut -d '|' -f3`; 
+if [ "$ver" != "$inc" ]
+ then echo "fixing: idincasso=$id, incassato.incass=$inc incassato.versamenti=$ver"
+psql smd -c "update incasso set incassato = $ver where id = $id;" 
+fi
+done
+echo "------------------"
+echo
+
 SQL_FIX_INCASSO_ABBONAMENTI_1="select 
    a.id as abbid,
    v.id as verid,
@@ -18,63 +81,7 @@ SQL_FIX_INCASSO_ABBONAMENTI_1="select
  and 
    a.importo+a.pregresso+a.spese+a.spese_estero < a.incassato;"
 
-SQL_GET_VERSAMENTI_DUPLICATI="select
-   a.id as abbid,
-   v.id as verid,
-   v.incasso_id as incid,
-   i.cassa,
-   a.code_line as abbcode,
-   v.operazione,
-   a.importo+a.pregresso+a.spese+a.spese_estero as totale,
-   a.incassato as abbinc ,
-   v.importo as verimp
- from abbonamento a
- left join versamento v
- on a.code_line = v.code_line
- left join incasso i
- on i.id = v.incasso_id
- where v.id is not null
- and a.incassato > 0
- and v.incassato = 0;"
-
-SQL_CHECK_IMPORTO="select i.id,
-       i.importo,
-       sum(v.importo) 
-   from incasso i left join versamento v 
-   on v.incasso_id=i.id  
-   group by i.id 
-   order by i.importo desc;"
-   
-SQL_CHECK_INCASSATO="select i.id,
-       i.incassato,
-       sum(v.incassato) 
-  from incasso i 
-  left join versamento v on v.incasso_id=i.id  
-  group by i.id 
-  order by i.incassato desc;"
-      
-psql smd -tAX -c "$SQL_CHECK_IMPORTO" | while read line
-do 
-id=`echo $line | cut -d '|' -f1` 
-inc=`echo $line | cut -d '|' -f2`; 
-ver=`echo $line | cut -d '|' -f3`; 
-if [ "$ver" != "$inc" ]
- then echo "importo errato $line"
- fi
-done
-
- 
-psql smd -tAX -c "$SQL_CHECK_INCASSATO" | while read line
-do 
-id=`echo $line | cut -d '|' -f1` 
-inc=`echo $line | cut -d '|' -f2`; 
-ver=`echo $line | cut -d '|' -f3`; 
-if [ "$ver" != "$inc" ]
- then echo "fixing incasso errato $line"
-psql smd -c "update incasso set incassato = $ver where id = $id;" 
-fi
-done
-
+echo check abbonamenti con incasso maggiore totale
 psql smd -tAX -c "$SQL_FIX_INCASSO_ABBONAMENTI_1" | while read line
 do
 abbid=`echo $line | cut -d '|' -f1` 
@@ -82,11 +89,13 @@ verid=`echo $line | cut -d '|' -f2`
 incid=`echo $line | cut -d '|' -f3`
 totale=`echo $line | cut -d '|' -f7`
 verinc=`echo $line | cut -d '|' -f10`
-echo fixing $line
+echo fixing: abbid=$abbid verid=$verid incid=$incid totale=$totale incassato=$verinc 
 psql smd -c "update abbonamento set incassato = $totale where id = $abbid;"
 psql smd -c "update versamento set incassato = $totale where id= $verid;"
 psql smd -c "update incasso set incassato= incassato - $verinc + $totale where id = $incid;"
 done 
+echo "------------------"
+echo
 
 echo check versamenti duplicati
 psql smd -tAX -c "$SQL_GET_VERSAMENTI_DUPLICATI"   
