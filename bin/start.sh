56ef7378af3c2be826bf1b7cc420f741de8ccb4f
Antonio <rssntn67@yahoo.it>
2020-01-22 06:45:34 +0100

    Fixed startup script

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 echo "Starting Gestione Abbonamenti ADP"
 mv ../logs/smd.log ../logs/smd`date +"%y%m%d-%H-%M"`.log
-nohup ../lib/smd-2.0-SNAPSHOT.jar > ../logs/smd.log 2>&1 &
+nohup ../lib/smd-2.0.jar > ../logs/smd.log 2>&1 &
 pid=`echo $!`
 sleep 3
 pids=`ps -f | grep $pid | grep -v grep | awk {'print $2'} | grep -v $pid` 
