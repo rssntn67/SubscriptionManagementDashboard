76e2ca560f52176830bd713d2c585ab4ad98d5a0
Antonio <rssntn67@yahoo.it>
2020-01-28 15:15:17 +0100

    Ready release 2.3
    Fixed sottointestazion and caption of
    EstrattoConto con abbonamento

--- src/main/java/it/arsinfo/smd/entity/AbbonamentoConEstrattoConto.java
+++ src/main/java/it/arsinfo/smd/entity/AbbonamentoConEstrattoConto.java
@@ -87,12 +87,16 @@ public class AbbonamentoConEstrattoConto implements SmdEntity {
     }
 
     public String getCaptionBrief() {
-        return intestatario.getCaptionBrief();
+        return String.format("'%s %s'", intestatario.getTitolo().getIntestazione(), intestatario.getDenominazione());
     }
 
     public String getSottoIntestazione() {
-        if (intestatario.getCo() == null) {
-            return "";
+        if (intestatario.getCo() == null ) {
+        	if (intestatario.getNome() != null) {
+        		return intestatario.getNome();
+        	} else {
+        		return "";
+        	}
         } 
         return "c/o" + intestatario.getCo().getCaptionBrief();
     }
