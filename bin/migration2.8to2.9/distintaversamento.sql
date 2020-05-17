c95e6dad5bf00bbb00817d635f523e341a30ec95
Antonio <rssntn67@yahoo.it>
2020-05-17 15:17:03 +0200

    iAdded update to set committente for versamento

--- bin/migration2.8to2.9/distintaversamento.sql
+++ bin/migration2.8to2.9/distintaversamento.sql
@@ -1,3 +1,14 @@
 ALTER TABLE incasso RENAME TO distinta_versamento;
 ALTER TABLE versamento RENAME COLUMN incasso_id TO distinta_versamento_id;
 
+UPDATE versamento v 
+SET committente_id = 
+  (SELECT a.intestatario_id FROM operazione_incasso oi 
+  LEFT JOIN abbonamento a 
+  ON a.id = oi.abbonamento_id 
+  WHERE oi.versamento_id = v.id 
+  AND oi.stato_operazione_incasso = 'Incasso'
+  limit 1) 
+WHERE committente_id is null 
+AND incassato > 0;
+  
