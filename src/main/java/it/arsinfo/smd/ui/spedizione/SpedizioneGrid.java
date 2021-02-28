61dec963a06372fa8d6b5e1f7a386cbe0e37d301
Antonio <rssntn67@yahoo.it>
2021-02-28 10:50:34 +0100

    Use Italian setting
    Migrated Spedizione

--- src/main/java/it/arsinfo/smd/ui/spedizione/SpedizioneGrid.java
+++ src/main/java/it/arsinfo/smd/ui/spedizione/SpedizioneGrid.java
@@ -3,19 +3,27 @@ package it.arsinfo.smd.ui.spedizione;
 import com.vaadin.ui.Grid;
 
 import it.arsinfo.smd.entity.Spedizione;
+import it.arsinfo.smd.ui.EuroConverter;
 import it.arsinfo.smd.ui.vaadin.SmdGrid;
 
 public class SpedizioneGrid extends SmdGrid<Spedizione> {
 
     public SpedizioneGrid(String gridname) {
         super(new Grid<>(Spedizione.class),gridname);
-        setColumns("destinazione",
+        getGrid().addColumn("destinazione");
+        getGrid().addColumn("meseSpedizione.nomeBreve").setCaption("Mese");
+        getGrid().addColumn("annoSpedizione.anno").setCaption("Anno");
+        getGrid().addColumn("pesoStimato");
+        getGrid().addColumn("spesePostali", EuroConverter.getEuroRenderer());
+        getGrid().addColumn("invioSpedizione").setCaption("Invio");
+
+        getGrid().setColumnOrder(
+        			"destinazione",
         		   "meseSpedizione",
         		   "annoSpedizione", 
         		   "pesoStimato",
                    "spesePostali",
                    "invioSpedizione"
                    );
-        setColumnCaption("invioSpedizione","Invio");
     }
 }
