7d6ae9930a240617eefbf6e5fd72825e63d33e43
Antonio <rssntn67@yahoo.it>
2021-02-28 10:20:24 +0100

    Use IT setting
    Fixed TrasportiCumulati

--- src/main/java/it/arsinfo/smd/ui/ddt/DocumentiTrasportoCumulatiGrid.java
+++ src/main/java/it/arsinfo/smd/ui/ddt/DocumentiTrasportoCumulatiGrid.java
@@ -3,15 +3,16 @@ package it.arsinfo.smd.ui.ddt;
 import com.vaadin.ui.Grid;
 
 import it.arsinfo.smd.entity.DocumentiTrasportoCumulati;
+import it.arsinfo.smd.ui.EuroConverter;
 import it.arsinfo.smd.ui.vaadin.SmdGrid;
 
 public class DocumentiTrasportoCumulatiGrid extends SmdGrid<DocumentiTrasportoCumulati>{
 
 	public DocumentiTrasportoCumulatiGrid(String gridName) {
 		super(new Grid<>(DocumentiTrasportoCumulati.class), gridName);
-        setColumns("anno.annoAsString","importo");
-     setColumnCaption("anno.annoAsString", "Anno");
-
+        getGrid().addColumn("anno.anno").setCaption("Anno");
+        getGrid().addColumn("importo",EuroConverter.getEuroRenderer());
+		getGrid().setColumnOrder("anno.anno","importo");
 	}
 	
 
