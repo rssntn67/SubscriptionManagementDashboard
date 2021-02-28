d43b0f5fa1b2e9d04ff7dce3f47a73db59499dbe
Antonio <rssntn67@yahoo.it>
2021-02-28 10:41:40 +0100

    use IT setting
    Migrated OfferteCumulate

--- src/main/java/it/arsinfo/smd/ui/offerta/OfferteCumulateGrid.java
+++ src/main/java/it/arsinfo/smd/ui/offerta/OfferteCumulateGrid.java
@@ -3,14 +3,17 @@ package it.arsinfo.smd.ui.offerta;
 import com.vaadin.ui.Grid;
 
 import it.arsinfo.smd.entity.OfferteCumulate;
+import it.arsinfo.smd.ui.EuroConverter;
 import it.arsinfo.smd.ui.vaadin.SmdGrid;
 
 public class OfferteCumulateGrid extends SmdGrid<OfferteCumulate>{
 
 	public OfferteCumulateGrid(String gridName) {
 		super(new Grid<>(OfferteCumulate.class), gridName);
-        setColumns("anno.annoAsString","importo");
-     setColumnCaption("anno.annoAsString", "Anno");
+        getGrid().addColumn("anno.anno").setCaption("Anno");
+        getGrid().addColumn("importo",EuroConverter.getEuroRenderer());
+
+        getGrid().setColumnOrder("anno.anno","importo");
 
 	}
 	
