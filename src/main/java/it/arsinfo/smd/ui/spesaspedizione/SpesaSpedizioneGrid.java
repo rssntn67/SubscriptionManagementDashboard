7dade904e5388fb1793870d0261de2f71cf8f9c8
Antonio <rssntn67@yahoo.it>
2021-02-28 10:58:53 +0100

    Use IT setting
    Migrated SpeseSpedizione

--- src/main/java/it/arsinfo/smd/ui/spesaspedizione/SpesaSpedizioneGrid.java
+++ src/main/java/it/arsinfo/smd/ui/spesaspedizione/SpesaSpedizioneGrid.java
@@ -3,16 +3,26 @@ package it.arsinfo.smd.ui.spesaspedizione;
 import com.vaadin.ui.Grid;
 
 import it.arsinfo.smd.entity.SpesaSpedizione;
+import it.arsinfo.smd.ui.EuroConverter;
 import it.arsinfo.smd.ui.vaadin.SmdGrid;
 
 public class SpesaSpedizioneGrid extends SmdGrid<SpesaSpedizione> {
 
     public SpesaSpedizioneGrid(String gridname) {
         super(new Grid<>(SpesaSpedizione.class),gridname);
-        setColumns("rangeSpeseSpedizione","areaSpedizione","spese","cor24h","cor3gg");
-        setColumnCaption("spese","Spese Postali");
-        setColumnCaption("cor24h","Corriere 24h");
-        setColumnCaption("cor3gg","Corriere 3gg");
+        getGrid().addColumn("rangeSpeseSpedizione").setCaption("Peso");
+        getGrid().addColumn("areaSpedizione").setCaption("Area");
+        getGrid().addColumn("spese",  EuroConverter.getEuroRenderer()).setCaption("Posta Ordin.");
+        getGrid().addColumn("cor24h", EuroConverter.getEuroRenderer()).setCaption("Corriere 24h");
+        getGrid().addColumn("cor3gg", EuroConverter.getEuroRenderer()).setCaption("Corriere 3gg");
+        
+        getGrid().setColumnOrder(
+        		"rangeSpeseSpedizione",
+        		"areaSpedizione",
+        		"spese",
+        		"cor24h",
+        		"cor3gg"
+        		);
     }
 
 }
