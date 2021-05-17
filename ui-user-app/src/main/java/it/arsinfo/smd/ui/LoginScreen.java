f5adda6c6eccc7728d72524989737878836df7b5
Antonio <rssntn67@yahoo.it>
2021-05-17 11:33:49 +0200

    Aggiunto google athentication

---
+++ ui-user-app/src/main/java/it/arsinfo/smd/ui/LoginScreen.java
@@ -0,0 +1,25 @@
+package it.arsinfo.smd.ui;
+
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+/**
+ * Adds an explicit link that the user has to click to login.
+ */
+@Route("login")
+@PageTitle("Login")
+public class LoginScreen extends FlexLayout {
+
+    private static final String URL = "/oauth2/authorization/google";
+
+    /**
+     * This methods gets the user into google sign in page.
+     */
+    public LoginScreen() {
+        Anchor googleLoginButton = new Anchor(URL, "Login with Google");
+        add(googleLoginButton);
+        setSizeFull();
+    }
+}
--- ui-user-app/src/main/java/it/arsinfo/smd/ui/MainLayout.java
+++ ui-user-app/src/main/java/it/arsinfo/smd/ui/MainLayout.java
@@ -3,19 +3,30 @@ package it.arsinfo.smd.ui;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
+import it.arsinfo.smd.data.UserSession;
 import it.arsinfo.smd.ui.anagrafica.AnagraficaView;
 import it.arsinfo.smd.ui.home.HomeView;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
 
 @CssImport("./styles/shared-styles.css")
 public class MainLayout extends AppLayout {
 
-    public MainLayout() {
+    @Autowired
+    UserSession userSession;
+
+    @PostConstruct
+    public void init() {
         createHeader();
         createDrawer();
     }
@@ -31,6 +42,17 @@ public class MainLayout extends AppLayout {
         header.setWidth("50%");
         header.addClassName("header");
 
+        Div div = new Div();
+        div.setText("Hello " + userSession.getUser().getFirstName() + " " + userSession.getUser().getLastName());
+        div.getElement().getStyle().set("font-size", "xx-large");
+
+        Image image = new Image(userSession.getUser().getPicture(), "User Image");
+
+        // Spring maps the 'logout' url so we should ignore it
+        Anchor logout = new Anchor("/logout", "Logout");
+        logout.getElement().setAttribute("router-ignore", true);
+
+        header.add(div,image,logout);
         addToNavbar(header);
 
     }
