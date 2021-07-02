349959146426fc1cc960249f52ac649159345482
Antonio <rssntn67@yahoo.it>
2021-07-02 10:46:55 +0200

    Fixed spedizioni

---
+++ ui-user-app/src/main/java/it/arsinfo/smd/ui/spedizione/IndirizzoForm.java
@@ -0,0 +1,87 @@
+package it.arsinfo.smd.ui.spedizione;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.shared.Registration;
+import it.arsinfo.smd.data.Paese;
+import it.arsinfo.smd.data.Provincia;
+import it.arsinfo.smd.service.dto.Indirizzo;
+
+import java.util.EnumSet;
+
+
+public class IndirizzoForm extends FormLayout {
+
+    private final Binder<Indirizzo> binder;
+    private Button close = new Button("Cancel");
+
+
+    public IndirizzoForm(Binder<Indirizzo> binder) {
+        this.binder = binder;
+        close.addClickListener(event -> fireEvent(new IndirizzoForm.CloseEvent(this)));
+        TextField intestazione = new TextField("Intestazione");
+        intestazione.setReadOnly(true);
+        TextField sottoIntestazione = new TextField("Sotto Intestazione");
+        sottoIntestazione.setReadOnly(true);
+        TextField indirizzo = new TextField("indirizzo");
+        indirizzo.setReadOnly(true);
+        TextField cap = new TextField("CAP");
+        cap.setReadOnly(true);
+        TextField citta = new TextField("Città");
+        citta.setReadOnly(true);
+        ComboBox<Provincia> provincia = new ComboBox<>("Provincia", EnumSet.allOf(Provincia.class));
+        provincia.setReadOnly(true);
+        ComboBox<Paese> paese = new ComboBox<>("Paese",
+                EnumSet.allOf(Paese.class));
+        paese.setReadOnly(true);
+
+        binder.forField(intestazione).bind(Indirizzo::getIntestazione,null);
+        binder.forField(sottoIntestazione).bind(Indirizzo::getSottoIntestazione,null);
+        binder.forField(indirizzo).bind(Indirizzo::getIndirizzo,null);
+        binder.forField(cap).bind(Indirizzo::getCap,null);
+        binder.forField(citta).bind(Indirizzo::getCitta,null);
+        binder.forField(provincia).bind(Indirizzo::getProvincia,null);
+        binder.forField(paese).bind(Indirizzo::getPaese,null);
+
+        add(createButtonsLayout());
+        add(intestazione);
+        add(sottoIntestazione);
+        add(indirizzo);
+        add(cap);
+        add(citta);
+        add(provincia);
+        add(paese);
+    }
+
+    public HorizontalLayout createButtonsLayout() {
+        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        close.addClickShortcut(Key.ESCAPE);
+        return new HorizontalLayout(close);
+    }
+
+    public void setEntity(Indirizzo indirizzo) {
+        binder.readBean(indirizzo);
+    }
+
+    public static class CloseEvent extends ComponentEvent<IndirizzoForm> {
+        CloseEvent(IndirizzoForm source) {
+            super(source,true);
+        }
+    }
+
+    public <K extends ComponentEvent<?>> Registration addListener(Class<K> eventType,
+                                                                  ComponentEventListener<K> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+
+}
--- ui-user-app/src/main/java/it/arsinfo/smd/ui/spedizione/SpedizioneView.java
+++ ui-user-app/src/main/java/it/arsinfo/smd/ui/spedizione/SpedizioneView.java
@@ -1,63 +1,92 @@
 package it.arsinfo.smd.ui.spedizione;
 
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import it.arsinfo.smd.dao.AnagraficaDao;
 import it.arsinfo.smd.data.Anno;
 import it.arsinfo.smd.data.Mese;
+import it.arsinfo.smd.entity.Anagrafica;
 import it.arsinfo.smd.entity.Spedizione;
 import it.arsinfo.smd.entity.SpedizioneItem;
 import it.arsinfo.smd.service.Smd;
 import it.arsinfo.smd.service.api.SpedizioneService;
+import it.arsinfo.smd.service.dto.Indirizzo;
 import it.arsinfo.smd.ui.MainLayout;
 import it.arsinfo.smd.ui.entity.EntityGridView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
-
-/**
- * A sample Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
- * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
- */
 
 @Route(value="adp/spedizione", layout = MainLayout.class)
 @PageTitle("Spedizione | ADP Portale")
-public class SpedizioneView extends EntityGridView<Spedizione> {
+public class SpedizioneView extends EntityGridView<SpedizioneItem> {
 
     private final SpedizioneService service;
-    public SpedizioneView(@Autowired SpedizioneService service) {
+    private final AnagraficaDao dao;
+    private final IndirizzoForm form = new IndirizzoForm(new BeanValidationBinder<>(Indirizzo.class));
 
+    public SpedizioneView(@Autowired SpedizioneService service, @Autowired AnagraficaDao anagraficaDao) {
         this.service=service;
+        this.dao = anagraficaDao;
 
     }
     @PostConstruct
     public void init() {
-        super.init(new Grid<>(Spedizione.class));
-        configureGrid("destinazione","pesoStimato");
-        setColumnCaption("invioSpedizione","Invio");
-        getGrid().addColumn(new NumberRenderer<>(Spedizione::getSpesePostali, Smd.getEuroCurrency())).setHeader("Spese Postali");
+        super.init(new Grid<>(SpedizioneItem.class));
+        configureGrid("statoSpedizione","numero");
+        setColumnCaption("pubbCaption","Pubblicazione");
+        setColumnCaption("spedizione.destinazione","Destinatario");
+        setColumnCaption("spedizione.pesoStimato","Peso");
+        setColumnCaption("spedizione.invioSpedizione","Invio");
+        getGrid().addColumn(new NumberRenderer<>(item-> item.getSpedizione().getSpesePostali(), Smd.getEuroCurrency())).setHeader("Spese Postali");
+
+
+        getGrid().asSingleSelect().addValueChangeListener(event ->
+                edit(event.getValue()));
+        form.addListener(IndirizzoForm.CloseEvent.class, e -> closeEditor());
 
         H2 header = new H2("Spedizioni di " + Mese.getMeseCorrente().getNomeBreve() + " " + Anno.getAnnoCorrente().getAnnoAsString());
-        add(header, getContent(getGrid()));
-
+        Div help = new Div();
+        help.setText("Seleziona spedizione per dettaglio indirizzo");
+        add(header, getContent(getGrid()),getContent(help),getContent(form));
+        form.setVisible(false);
         updateList();
     }
 
+    public void edit(SpedizioneItem entity) {
+        Anagrafica destinatario = entity.getSpedizione().getDestinatario();
+        Indirizzo indirizzo = Indirizzo.getIndirizzo(destinatario);
+        if (destinatario.getCo() != null ) {
+            Anagrafica co = dao.findById(destinatario.getCo().getId()).orElse(null);
+            Indirizzo.getIndirizzo(destinatario,co);
+        }
+        form.setEntity(indirizzo);
+        form.setVisible(true);
+        addClassName("editing");
+    }
+
+    public void closeEditor() {
+        form.setEntity(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
     @Override
-    public List<Spedizione> filter() {
-        return service.searchBy(getUserSession().getLoggedInIntestatario(), Anno.getAnnoCorrente(), Mese.getMeseCorrente());
+    public List<SpedizioneItem> filter() {
+        List<SpedizioneItem> items = new ArrayList<>();
+        for (Spedizione s: service.searchBy(getUserSession().getLoggedInIntestatario(), Anno.getAnnoCorrente(), Mese.getMeseCorrente())) {
+            for (SpedizioneItem item: service.getItems(s)) {
+                item.setSpedizione(s);
+                items.add(item);
+            }
+        }
+        return items;
     }
 }
