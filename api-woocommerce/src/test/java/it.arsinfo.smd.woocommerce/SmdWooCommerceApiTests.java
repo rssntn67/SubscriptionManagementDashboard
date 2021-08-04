c3400ffc18e9f177c9be84aab39a7dfa47d08cc9
Antonio <rssntn67@yahoo.it>
2021-08-04 22:06:44 +0200

    Added more tests for wooapi

--- api-woocommerce/src/test/java/it.arsinfo.smd.woocommerce/SmdWooCommerceApiTests.java
+++ api-woocommerce/src/test/java/it.arsinfo.smd.woocommerce/SmdWooCommerceApiTests.java
@@ -29,4 +29,34 @@ public class SmdWooCommerceApiTests {
         System.out.println(products.size());
     }
 
+    @Test
+    public void getCustomersTest() {
+
+        OAuthConfig config = new OAuthConfig("http://www.retepreghierapapa.it", "ck_f70f8d7811e6a176cf58da451df59960d68244b0", "cs_1bacae59ede6f326690e2855f108d844a397327b");
+        WooCommerce wooCommerce = new WooCommerceAPI(config, ApiVersionType.V3);
+
+        // Get all with request parameters
+        Map<String, String> params = new HashMap<>();
+        params.put("per_page","100");
+        params.put("offset","0");
+        List customers = wooCommerce.getAll(EndpointBaseType.CUSTOMERS.getValue(), params);
+        customers.forEach(p -> System.out.println(p));
+        System.out.println(customers.size());
+    }
+
+    @Test
+    public void getOrdersTest() {
+
+        OAuthConfig config = new OAuthConfig("http://www.retepreghierapapa.it", "ck_f70f8d7811e6a176cf58da451df59960d68244b0", "cs_1bacae59ede6f326690e2855f108d844a397327b");
+        WooCommerce wooCommerce = new WooCommerceAPI(config, ApiVersionType.V3);
+
+        // Get all with request parameters
+        Map<String, String> params = new HashMap<>();
+        params.put("per_page","100");
+        params.put("offset","0");
+        List orders = wooCommerce.getAll(EndpointBaseType.ORDERS.getValue(), params);
+        orders.forEach(p -> System.out.println(p));
+        System.out.println(orders.size());
+    }
+
 }
